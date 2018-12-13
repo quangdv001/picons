a3794982434e83f8d7d4c81f9dcbfeb489452280
ThanhPK <thanhpk@vccloud.vn>
2018-12-13 17:11:26 +0700

    push

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,7 @@ define('DB_CHARSET', 'utf8mb4');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 define('WP_MEMORY_LIMIT', '256M');
+define('FS_METHOD', 'direct');
 /**#@+
  * Authentication Unique Keys and Salts.
  *
